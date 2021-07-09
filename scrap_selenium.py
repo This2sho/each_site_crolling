a963f781c473a777cecb41ede2e94037571d57ed
This2sho <zx00018@naver.com>
2021-07-10 01:21:03 +0900

    +scraping

--- scrap_selenium.py
+++ scrap_selenium.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import json
+import time
 
 options = webdriver.ChromeOptions()
 options.headless = True
@@ -10,8 +11,8 @@ options.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.
 browser = webdriver.Chrome(options=options)
 browser.maximize_window()
 
-url = "https://codegreen.io/shop"
-name = "codegreen"
+url = "https://cueclyp.com/new"
+name = "cueclyp"
 
 # 저장할 파일 설정
 file_name = "data.json"
@@ -23,24 +24,27 @@ logo_d = dict()
 browser.get(url)
 soup = BeautifulSoup(browser.page_source, "lxml")
 
+# with open("test.html", "w", encoding='utf-8') as f:
+#     f.write(soup.prettify())
+
 # 필요한 데이터
 # 로고 
 # 상품 사진, 이름, 가격, 링크
-item_url = "https://codegreen.io"
-logo = item_url+ soup.find("div", attrs={"class":"logo_mobile"}).img["src"]
+item_url = "https://contents.sixshop.com"
+logo = soup.find("a", attrs={"href":"/"}).img["src"]
 logo_d["logo"] = logo
 temp_list.append(logo_d)
 
-sells = soup.find_all("a", attrs={"class":"card_badge_best--wrapper"})
+sells = soup.find_all("div", attrs={"class":"shopProductWrapper"})
 
 for idx, sell in enumerate(sells):
     if(idx >= 30):
         break
     item = dict()
-    item["img"] = sell.img["src"]
-    item["name"] = sell.next_sibling.h4.get_text()
-    item["price"] = sell.next_sibling.div.span.get_text()
-    item["link"] = item_url+ sell["href"]
+    item["img"] = item_url+sell.find("div", attrs={"class":"thumb img"})["imgsrc"]
+    item["link"] = item_url+sell.a["href"]
+    item["name"] = sell.find("div", attrs={"class":"shopProduct productName"}).get_text()
+    item["price"] = sell.find("span", attrs={"class":"productPriceSpan"}).get_text()
     temp_list.append(item)
 
 # 잘 써졌는지 확인용
