9e2aac8c02f777a6e299d63a9993c974292d87f7
This2sho <zx00018@naver.com>
2021-07-20 18:19:34 +0900

    edit error

--- cueclyp.py
+++ cueclyp.py
@@ -1,58 +1,62 @@
+from os import write
+import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
 import json
-import time
 
-options = webdriver.ChromeOptions()
-options.headless = True
-options.add_argument("windw-size=1902x1080") # pc해상도 설정
-options.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-
-browser = webdriver.Chrome(options=options)
-browser.maximize_window()
-
-url = "https://cueclyp.com/new"
-name = "cueclyp"
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
 
 # 저장할 파일 설정
 file_name = "data.json"
 
 data = dict() # 맨 처음 파일 쓸 때 씀. + test 용
 temp_list = []
-logo_d = dict()
+brand = dict()
 
-browser.get(url)
-soup = BeautifulSoup(browser.page_source, "lxml")
+url = "https://cueclyp.com/"
+name = "cueclyp"
+
+res = requests.get(url+"aboutcueclyp", headers=headers)
+res.raise_for_status()
+# res.encoding = 'euc-kr'
+soup = BeautifulSoup(res.text, "lxml")
 
 # with open("test.html", "w", encoding='utf-8') as f:
 #     f.write(soup.prettify())
 
 # 필요한 데이터
 # 로고 
+# 브랜드 설명
 # 상품 사진, 이름, 가격, 링크
-item_url = "https://contents.sixshop.com"
 logo = soup.find("a", attrs={"href":"/"}).img["src"]
-logo_d["logo"] = logo
-temp_list.append(logo_d)
+description = soup.find("div", attrs={"class":"item-element"}).get_text()
+brand["logo"] = logo
+brand["description"] = description
+temp_list.append(brand)
+
+res = requests.get(url+"new", headers=headers)
+res.raise_for_status()
+# res.encoding = 'euc-kr'
+soup = BeautifulSoup(res.text, "lxml")
 
 sells = soup.find_all("div", attrs={"class":"shopProductWrapper"})
-
+img_url = "https://contents.sixshop.com"
+item_url = "https://cueclyp.com"
 for idx, sell in enumerate(sells):
     if(idx >= 30):
         break
     item = dict()
-    item["img"] = item_url+sell.find("div", attrs={"class":"thumb img"})["imgsrc"]
-    item["link"] = item_url+sell.a["href"]
-    item["name"] = sell.find("div", attrs={"class":"shopProduct productName"}).get_text()
-    item["price"] = sell.find("span", attrs={"class":"productPriceSpan"}).get_text()
+    item["img"] = img_url + sell.find("div", attrs={"class":"img"})["imgsrc"]
+    item["link"] = item_url + sell.a["href"]
+    item["name"] = sell.a["alt"]
+    item["price"] = sell.find("div", attrs={"class":"shopProduct price"}).get_text()
     temp_list.append(item)
 
 # 잘 써졌는지 확인용
 data[name] = temp_list
 with open("test.json", "w", encoding="utf-8") as make_file:
     json.dump(data, make_file, indent="\t", ensure_ascii=False)
-
-
 
 # 파일 쓰기 test보고 잘 됐으면 ㄱㄱ
 # 저장된 파일에 추가하기
@@ -62,5 +66,4 @@ with open("test.json", "w", encoding="utf-8") as make_file:
 # with open(file_name, "w", encoding="utf-8") as make_file:
 #     json.dump(read_data, make_file, indent="\t",ensure_ascii=False)
 
-browser.quit()
 print("완료")
--- ulkin.py
+++ ulkin.py
@@ -8,8 +8,8 @@ headers = {
     }
 
 
-url = "http://heureux.kr/shop/shopbrand.html?type=N&xcode=005&mcode=003"
-name = "heureux"
+url = "https://ulkin.co.kr/product-category/unisex?orderby=popularity"
+name = "ulkin"
 
 # 저장할 파일 설정
 file_name = "data.json"
@@ -20,30 +20,30 @@ logo_d = dict()
 
 res = requests.get(url, headers=headers)
 res.raise_for_status()
-res.encoding = 'euc-kr'
+# res.encoding = 'euc-kr'
 soup = BeautifulSoup(res.text, "lxml")
 
-with open("test.html", "w", encoding='utf-8') as f:
-    f.write(soup.prettify())
+# with open("test.html", "w", encoding='utf-8') as f:
+#     f.write(soup.prettify())
 
 # 필요한 데이터
 # 로고 
 # 상품 사진, 이름, 가격, 링크
 item_url = "http://heureux.kr"
-logo = item_url+ soup.find("a", attrs={"href":"/index.html"}).img["src"]
+logo = soup.find("a", attrs={"href":"https://ulkin.co.kr/"}).img["src"]
 logo_d["logo"] = logo
 temp_list.append(logo_d)
 
-sells = soup.find_all("dl", attrs={"class":"item-list plan-list"})
+sells = soup.find_all("div", attrs={"class":"col-inner"})
 
 for idx, sell in enumerate(sells):
-    if(idx >= 29):
+    if(idx >= 30):
         break
     item = dict()
-    item["img"] = sell.dt.a.img["src"]
-    item["name"] = sell.find("li", attrs={"prd-name"}).get_text()
-    item["price"] = sell.find("p").get_text()
-    item["link"] = item_url+ sell.dt.a["href"]
+    item["img"] = sell.find("div", attrs={"class":"image-fade_in_back"}).a.img["src"]
+    item["link"] = sell.find("div", attrs={"class":"image-fade_in_back"}).a["href"]
+    item["name"] = sell.find("div", attrs={"class":"title-wrapper"}).get_text()
+    item["price"] = sell.find("div", attrs={"class":"price-wrapper"}).get_text().split("\n")[1]
     temp_list.append(item)
 
 # 잘 써졌는지 확인용
@@ -58,4 +58,5 @@ with open(file_name, 'r', encoding="utf-8") as f:
 read_data[name] = temp_list
 with open(file_name, "w", encoding="utf-8") as make_file:
     json.dump(read_data, make_file, indent="\t",ensure_ascii=False)
+
 print("완료")
