884eaee6677b92093e1c90efa40463bd2eac95d0
This2sho <zx00018@naver.com>
2021-07-29 13:45:55 +0900

    add data 'pangaia'

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,5 @@
 
-with open("test.json", "w", encoding="utf-8") as make_file:
-    json.dump(data, make_file, indent="\t", ensure_ascii=False)
+    read_data = json.load(f)
+read_data[name] = temp_list
+with open(file_name, "w", encoding="utf-8") as make_file:
+    json.dump(read_data, make_file, indent="\t",ensure_ascii=False)
